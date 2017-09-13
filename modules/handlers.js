5750b16d7b137001aae6aea18b5fc3a0bf8e8c7d
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.internal.salesforce.com>
2017-09-13 16:21:09 -0500

    Busca rut

--- modules/handlers.js
+++ modules/handlers.js
@@ -31,7 +31,7 @@ exports.buscaContacto = (sender, values) => {
 exports.validaNombre = (sender, values) => {
 	console.log('Contacto valida Nombre: %j', contacto);
 	if(contacto){
-		var nombre = contacto.firstname + ' ' + contacto.lastname;
+		var nombre = contacto.get('firstname') + ' ' + contacto.get('lastname');
 		console.log('Nombre contacto: ' + nombre);
 		if(nombre == values[0]){
 			messenger.send({text : `Muy bien! Por último, proporcióname tu token (multipass)`}, sender);
