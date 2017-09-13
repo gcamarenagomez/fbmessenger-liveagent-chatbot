521925d2c81ef699b65a9b40ef8d223b32413b86
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.internal.salesforce.com>
2017-09-13 12:24:45 -0500

    Initial deploy

---
+++ modules/salesforce.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let nforce = require('nforce'),
+	SF_CLIENT_ID = process.env.SF_CLIENT_ID,
+	SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET,
+	SF_USER_NAME = process.env.SF_USER_NAME,
+	SF_PASSWORD = process.env.SF_PASSWORD;
+
+let org = nforce.createConnection({
+	clientId : SF_CLIENT_ID,
+	clientSecret : SF_CLIENT_SECRET,
+	redirectUri : 'http://localhost:8200/oauth/_callback',
+	mode : 'single',
+	autoRefresh : true
+});
+
+let login = () => {
+	org.authenticate({username : SF_USER_NAME, password : SF_PASSWORD}, err => {
+		if(err){
+			console.error('Authentication error');
+			console.error(err);
+		}
+		else{
+			console.log('Authentication successful');
+		}
+	});
+};
+
+login();
+
+exports.org = org;
