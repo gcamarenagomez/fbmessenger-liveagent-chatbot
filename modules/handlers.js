2d5c9d5e12162bcaa143c8a7644615af4de24594
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.internal.salesforce.com>
2017-09-13 16:00:27 -0500

    Busca rut

--- modules/handlers.js
+++ modules/handlers.js
@@ -4,6 +4,8 @@ let salesforce = require('./salesforce'),
 	messenger = require('./messenger'),
 	formatter = require('./formatter');
 
+var rut;
+
 exports.hola = (sender) => {
 	messenger.getUserInfo(sender).then(response => {
 		messenger.send({text : `Hola ${response.first_name}! Por favor proporcióname tu RUT sin puntos ni guiones.`}, sender);
@@ -16,8 +18,10 @@ exports.ayuda = (sender) => {
 
 exports.buscaContacto = (sender, values) => {
 	console.log('Valor recibido: ' + values[0]);
+	rut = values[0];
 	messenger.send({text : `Gracias! Estoy validando tu RUT en nuestro sistema...`}, sender);
 	salesforce.findContact(values[0]).then(contact => {
+		console.log('Variable global RUT: ' + rut);
 		messenger.send({text : `Listo! Por favor indícame tu nombre completo.`}, sender);
 	})
 }
