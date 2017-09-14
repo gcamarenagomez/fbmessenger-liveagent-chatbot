32585fd41d8f320d444cb1441595be2a63fe84b4
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.internal.salesforce.com>
2017-09-14 14:55:15 -0500

    poll for agent message

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -54,7 +54,7 @@ exports.start_chat = (sender, values) => {
 				globalSequence = globalSequence + 1;
 				console.log('Global Sequence ' + globalSequence);
 				message(globalSession, globalSequence, sender);
-			}, 3500);
+			}, 5000);
 			//message(session, -1, sender);
 		});
 	});
